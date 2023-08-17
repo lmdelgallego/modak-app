c9cf7a55d3e6dfe27422e3f6e1617a44eba89b82
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-08-17 15:16:45 -0500

    ✨feature: Card Design

--- src/app/artwork/artwork.module.ts
+++ src/app/artwork/artwork.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { AnimateModule } from 'primeng/animate';
+
 import { ArtworkComponent } from './artwork.component';
 import { ArtworkRoutingModule } from './artwork-routing.module';
 
 @NgModule({
   declarations: [ArtworkComponent],
-  imports: [CommonModule, ArtworkRoutingModule],
+  imports: [CommonModule, ArtworkRoutingModule, AnimateModule, InputTextModule, ButtonModule],
 })
 export class ArtworkModule {}
